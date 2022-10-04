8fde321ff472ec3d55fc57e1c652e97a5fa71294
Michael Adams <unquietwiki@gmail.com>
2022-10-03 19:52:22 -0700

    Initial repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# sortplz
+
+_sortplz_ is a simple file sorter. It takes targeted files, and moves them to subdirectories for further review.
---
+++ src/pkg/config.nim
@@ -0,0 +1,5 @@
+const
+  pkgTitle*       = "sortplz"
+  pkgVersion*     = "0.1.0"
+  pkgAuthor*      = "Michael Adams"
+  pkgDescription* = "Sort files into subdirectories, based on given criteria."
